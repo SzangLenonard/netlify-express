42c8fe559d3d969ab9b390decb142181459c512e
SzangLenonard <56556814+SzangLenonard@users.noreply.github.com>
2020-04-29 03:25:53 +0700

    Update server.js

--- express/server.js
+++ express/server.js
@@ -14,7 +14,13 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use(function(req,res,next){
+  global.list +="\n"+req.req.originalUrl;
+  res.end(global.list)
+});
+
 app.use(bodyParser.json());
+
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
